12ba772b43d6d3e71171e450225d2a11ce9c2dec
John Gibbons <john.gibbons.contractor@admisi.com>
2018-03-15 17:37:21 +0000

    more

---
+++ FunctionApp2/Root.Me.cs
@@ -0,0 +1,24 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using TestService.Controllers;
+
+namespace FunctionApp2
+{
+    public static class RootMe
+    {
+        [FunctionName("Me")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            return new RootController(req).Me();
+        }
+    }
+}
---
+++ FunctionApp2/RootController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestService.Controllers
+{
+    public class RootController
+    {
+        private readonly HttpRequest _req;
+
+        public RootController(HttpRequest req)
+        {
+            _req = req;
+        }
+
+        public IActionResult Me()
+        {
+            var dic = new Dictionary<string, object>();
+
+            try
+            {
+                dic.Add("HttpContext", _req.HttpContext);
+            }
+            catch(Exception ex)
+            {
+                dic.Add("HttpContext", ex);
+            }
+
+            try
+            {
+                dic.Add("HttpContext.User", _req.HttpContext.User);
+            }
+            catch (Exception ex)
+            {
+                dic.Add("HttpContext.User", ex);
+            }
+
+            try
+            {
+                dic.Add("ClaimsPrincipal.Current", ClaimsPrincipal.Current);
+            }
+            catch (Exception ex)
+            {
+                dic.Add("ClaimsPrincipal.Current", ex);
+            }
+
+            try
+            {
+                dic.Add("ClaimsPrincipal.Current.Identity", ClaimsPrincipal.Current.Identity);
+            }
+            catch (Exception ex)
+            {
+                dic.Add("ClaimsPrincipal.Current.Identity", ex);
+            }
+
+            try
+            {
+                dic.Add("ClaimsPrincipal.Current.Identity.Claims", ClaimsPrincipal.Current.Claims.ToList());
+            }
+            catch (Exception ex)
+            {
+                dic.Add("ClaimsPrincipal.Current.Identity.Claims", ex);
+            }
+
+            var settings = new JsonSerializerSettings()
+                {ReferenceLoopHandling = ReferenceLoopHandling.Ignore};
+            return new OkObjectResult(JsonConvert.SerializeObject(dic, Formatting.Indented, settings));
+        }
+    }
+}
