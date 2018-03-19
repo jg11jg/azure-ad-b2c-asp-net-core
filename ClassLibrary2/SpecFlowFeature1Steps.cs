1f3b9d0b6e2a977daf9b65ee65b48733c440c465
John Gibbons <jg11jg@outlook.com>
2018-03-19 02:25:49 +0000

    xunit and moq

---
+++ ClassLibrary2/SpecFlowFeature1Steps.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using FunctionApp2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Moq;
+using TechTalk.SpecFlow;
+
+namespace ClassLibrary2
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            var log = new Mock<TraceWriter>(TraceLevel.Info);
+
+            var req = new Mock<HttpRequest>();
+
+            var rv = Function1.Run(req.Object, log.Object);
+            //ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            //ScenarioContext.Current.Pending();
+        }
+    }
+}
